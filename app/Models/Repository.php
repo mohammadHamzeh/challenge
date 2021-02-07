a5a24d28451ff857d536a119221ac1fcc8e3d42a
Mohammad <mr.hamze00@gmail.com>
2021-02-07 22:24:18 +0330

    create model and migration Repository

---
+++ app/Models/Repository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Repository extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "repository_id",
+        "repository_name",
+        "description",
+        "url",
+        "language",
+    ];
+}
