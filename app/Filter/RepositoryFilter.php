00cb843931b9a5f5942f92910ecfe53049d4068d
Mohammad <mr.hamze00@gmail.com>
2021-02-08 20:55:24 +0330

    create tag filter in RepositoryFilter

--- app/Filter/RepositoryFilter.php
+++ app/Filter/RepositoryFilter.php
@@ -4,7 +4,14 @@ namespace App\Filter;
 
 
 use App\Filter\Contracts\QueryFilter;
+use Illuminate\Database\Eloquent\Builder;
 
 class RepositoryFilter extends QueryFilter
 {
+    public function tags($value)
+    {
+        return $this->builder->whereHas('tags', function (Builder $query) use ($value) {
+            $query->where('slug', 'like', $value);
+        })->get();
+    }
 }
